1a13fc7a5f9713bddf2fa6e85f868f74e76ec037
GideonLeebleh <90928962+GideonLeebleh@users.noreply.github.com>
2021-11-08 12:50:30 -0500

    Update addPairs.java

--- assignment1/addPairs.java
+++ assignment1/addPairs.java
@@ -1,3 +1,10 @@
+/**
+*   Author name: Gideon Lee
+*   Date: Nov 8 2021
+*   Name of program: AddPairs
+*   Program puropose: This program will divide a set of numbers into pairs and then output the sum of the set of numbers.
+*                     If the input is an odd number of digits, the last digit will be added with zero instead.
+*/
 package com.company;
 import java.util.Scanner;
 
@@ -7,84 +14,84 @@ public class addPairs {
 
         //setup
 
-            Scanner in = new Scanner(System.in);
-            String input;
-            int integer;
-            int var1 = 0;
-            int var2 = 0;
-            int var3 = 0;
-            boolean bool = true;
+        Scanner in = new Scanner(System.in);
+        String input;
+        int integer;
+        int var1 = 0;
+        int var2 = 0;
+        int var3 = 0;
+        boolean bool = true;
 
         //input
 
-            System.out.print("Enter a string of integers: ");
-            integer = in.nextInt();
+        System.out.print("Enter a string of integers: ");
+        integer = in.nextInt();
 
         //calculations
 
-            System.out.print("("); //apart of the overall output
+        System.out.print("("); //apart of the overall output
 
-            //changes int into string
-            input = Integer.toString(integer);
+        //changes int into string
+        input = Integer.toString(integer);
 
-            //finds if there is an odd digit number
-            var2 = input.length() % 2;
+        //finds if there is an odd digit number
+        var2 = input.length() % 2;
 
-            //if the inputted value has an odd digit out
-            if(var2 != 0)
+        //if the inputted value has an odd digit out
+        if(var2 != 0)
+        {
+            //puts int into an array
+            int[] nums = new int [input.length() + 1];
+            for(int i = 0; i <= input.length() - 1; i++)
             {
-                    //puts int into an array
-                    int[] nums = new int [input.length() + 1];
-                    for(int i = 0; i <= input.length() - 1; i++)
-                    {
-                            nums[i] = Character.getNumericValue(input.charAt(i));
-                            var2 = nums[input.length() - 1];
-                            nums[input.length() - 1] = 0;
-                            nums[input.length()] = var2;
-                    }
-
-                    //puts numbers into pairs
-                    int [] sums = new int[input.length()];
-                    for(int i = 0; i <= input.length() - 1; i += 2)
-                    {
-                            var1 = nums[i] * 10;
-                            sums[i] = var1 + nums[i +  1];
-                            if(i == input.length() - 1)
-                            {
-                                    System.out.print(sums[i] + ")");
-                            }
-                            else {System.out.print(sums[i] + "+");}
-
-                            var3 = var3 + sums[i];
-                    }
-                    System.out.print(" = " + var3);
+                nums[i] = Character.getNumericValue(input.charAt(i));
+                var2 = nums[input.length() - 1];
+                nums[input.length() - 1] = 0;
+                nums[input.length()] = var2;
             }
 
-            //if there is no odd digit out
-            else if (var2 == 0)
+            //puts numbers into pairs
+            int [] sums = new int[input.length()];
+            for(int i = 0; i <= input.length() - 1; i += 2)
             {
-                    int[] nums = new int[input.length()];
-                    //puts int into an array
-                    for (int i = 0; i <= input.length() - 1; i++)
-                    {
-                            nums[i] = Character.getNumericValue(input.charAt(i));
-                    }
+                var1 = nums[i] * 10;
+                sums[i] = var1 + nums[i +  1];
+                if(i == input.length() - 1)
+                {
+                    System.out.print(sums[i] + ")");
+                }
+                else {System.out.print(sums[i] + "+");}
 
-                    //puts numbers into pairs
-                    int [] sums = new int[input.length()];
-                    for(int i = 0; i <= input.length() - 1; i += 2)
-                    {
-                            var1 = nums[i] * 10;
-                            sums[i] = var1 + nums[i +  1];
-                            if(i == input.length() - 2)
-                            {
-                                    System.out.print(sums[i] + ")");
-                            }
-                            else {System.out.print(sums[i] + "+");}
-
-                            var3 = var3 + sums[i];
-                    }
-                    System.out.println(" = " + var3);
+                var3 = var3 + sums[i];
             }
+            System.out.print(" = " + var3);
+        }
+
+        //if there is no odd digit out
+        else if (var2 == 0)
+        {
+            int[] nums = new int[input.length()];
+            //puts int into an array
+            for (int i = 0; i <= input.length() - 1; i++)
+            {
+                nums[i] = Character.getNumericValue(input.charAt(i));
+            }
+
+            //puts numbers into pairs
+            int [] sums = new int[input.length()];
+            for(int i = 0; i <= input.length() - 1; i += 2)
+            {
+                var1 = nums[i] * 10;
+                sums[i] = var1 + nums[i +  1];
+                if(i == input.length() - 2)
+                {
+                    System.out.print(sums[i] + ")");
+                }
+                else {System.out.print(sums[i] + "+");}
+
+            var3 = var3 + sums[i];
+            }
+        System.out.println(" = " + var3);
+    }
     }
 }
