1f9903ecbeadd534b247d51f2835919e6955a4f9
GideonLeebleh <90928962+GideonLeebleh@users.noreply.github.com>
2021-11-08 13:26:16 -0500

    Update leastPower.java

--- assignment1/leastPower.java
+++ assignment1/leastPower.java
@@ -1,49 +1,55 @@
+/**
+*   Author name: Gideon Lee
+*   Date: Nov 8th 2021
+*   Program name: LeastPower
+*   Program purpose: This is a program that reads a positive integer and then finds the smallest power of two that is greater than or equal to the number that are read.
+*/
+
 package com.company;
 import java.util.Scanner;
 
-public class leastPower {
+public class LeastPower {
 
     public static void main(String[] args) {
 
         //setup
 
-            Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-            //variables
-            int num = 0;
-            double num2 = 0;
-            int pow = 0;
-            boolean bool = false;
+        //variables
+        int num = 0;
+        double num2 = 0;
+        int pow = 0;
+        boolean bool = false;
 
 
         //Input
 
-            while (!bool)
+        while (!bool)
+        {
+            System.out.print("Input a number ");
+            num = in.nextInt();
+
+            if (num <= 0)
             {
-                System.out.print("Input a number ");
-                num = in.nextInt();
-
-                if (num <= 0)
-                {
-                    System.out.println("Nope, needs to be a positive integer. Try again!");
-                    System.out.println();
-                }
-
-                else {bool = true;}
+                System.out.println("Nope, needs to be a positive integer. Try again!");
+                System.out.println();
             }
+            else {bool = true;}
+        }
 
 
         //Calculations
 
-            for (int i = 0; num >= num2; i++)
-            {
-                num2 = Math.pow(2, i);
-                pow = i;
-            }
+        for (int i = 0; num >= num2; i++)
+        {
+            num2 = Math.pow(2, i);
+            pow = i;
+        }
 
 
         // Output
 
-            System.out.println( num2 + " = 2^" + pow + " is the smallest power of 2 ");
+        System.out.println( num2 + " = 2^" + pow + " is the smallest power of 2 ");
     }
 }
