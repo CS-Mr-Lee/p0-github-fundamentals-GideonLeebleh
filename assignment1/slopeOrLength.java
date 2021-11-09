d0572c6ebb331b02207f210478be5196975230b1
GideonLeebleh <90928962+GideonLeebleh@users.noreply.github.com>
2021-11-09 13:15:40 -0500

    Update slopeOrLength.java

--- assignment1/slopeOrLength.java
+++ assignment1/slopeOrLength.java
@@ -1,7 +1,14 @@
+/**
+ *   Author name: Gideon Lee
+ *   Date: Nov 9 2021
+ *   Program name: SlopeOrLength
+ *   Program purpose: This is a program that contains 2 methods, one that takes the coordinates of a line segement and returns the length of the line to the user and
+ *                    a method that takes the coordinates of a line segment and returns the slope.
+*/
 package com.company;
 import  java.util.Scanner;
 
-public class slopeOrLength {
+public class SlopeOrLength {
 
     //length method
     public static double length(int x1, int x2, int y1, int y2)
@@ -25,54 +32,53 @@ public class slopeOrLength {
 
         //setup
 
-            Scanner in = new Scanner(System.in);
-            String input;
-            int x1;
-            int x2;
-            int y1;
-            int y2;
+        Scanner in = new Scanner(System.in);
+        String input;
+        int x1;
+        int x2;
+        int y1;
+        int y2;
 
 
         //input
 
-            System.out.print("length or slope : ");
-            input = in.nextLine();
+        System.out.print("length or slope : ");
+        input = in.nextLine();
 
-            if(input.equals("length"))
-            {
-                System.out.println("Input a x1, y1, x2, and y2 coordinate: ");
-                System.out.print("x1 = ");
-                x1 = in.nextInt();
-                System.out.print("x2 = ");
-                x2 = in.nextInt();
-                System.out.print("y1 = ");
-                y1 = in.nextInt();
-                System.out.print("y2 = ");
-                y2 = in.nextInt();
+        if(input.equals("length"))
+        {
+            System.out.println("Input a x1, y1, x2, and y2 coordinate: ");
+            System.out.print("x1 = ");
+            x1 = in.nextInt();
+            System.out.print("x2 = ");
+            x2 = in.nextInt();
+            System.out.print("y1 = ");
+            y1 = in.nextInt();
+            System.out.print("y2 = ");
+            y2 = in.nextInt();
 
-                double num = length(x1,x2,y1,y2);
+            double num = length(x1,x2,y1,y2);
 
-                //output
-                System.out.println("The length is: " + num);
-            }
+            //output
+            System.out.println("The length is: " + num);
+        }
+        else if(input.equals("slope"))
+        {
+            System.out.println("Input a x1, y1, x2, and y2 coordinate: ");
+            System.out.print("x1 = ");
+            x1 = in.nextInt();
+            System.out.print("x2 = ");
+            x2 = in.nextInt();
+            System.out.print("y1 = ");
+            y1 = in.nextInt();
+            System.out.print("y2 = ");
+            y2 = in.nextInt();
 
-            else if(input.equals("slope"))
-            {
-                System.out.println("Input a x1, y1, x2, and y2 coordinate: ");
-                System.out.print("x1 = ");
-                x1 = in.nextInt();
-                System.out.print("x2 = ");
-                x2 = in.nextInt();
-                System.out.print("y1 = ");
-                y1 = in.nextInt();
-                System.out.print("y2 = ");
-                y2 = in.nextInt();
+            double num = slope(x1,x2,y1,y2);
 
-                double num = slope(x1,x2,y1,y2);
-
-                //output
-                System.out.println("The slope is: " + num);
-            }
+            //output
+            System.out.println("The slope is: " + num);
+        }
 
     }
 }
