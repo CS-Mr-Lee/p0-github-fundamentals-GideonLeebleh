65151d8f298c8e199a8f4dc1659f01092904f894
GideonLeebleh <90928962+GideonLeebleh@users.noreply.github.com>
2021-11-09 13:24:32 -0500

    Update foolProof.java

--- assignment1/foolProof.java
+++ assignment1/foolProof.java
@@ -1,17 +1,28 @@
+/**
+ *   Author name: Gideon Lee
+ *   Date: Nov 9 2021
+ *   Program name: FoolProof
+ *   Program purpose: This is a program that will repeatedly ask for the numerator and divisor. For each set of data, the program prints out the result (quotient), or 
+ *   an informative error message if there is a problem (division by zero or poor input data). The program continues looping, even if there is a problem. 
+ *   Exit the loop when data entered for the numerator start with characters “q” or “Q”. Don’t print out an error message in this case. Don’t ask for the divisor 
+ *   if the user just asked to quit.
+
+
+ */
 package com.company;
 import java.util.Scanner;
 
-public class foolProof {
+public class FoolProof {
 
     public static void main(String[] args) {
 
         //setup
 
-            Scanner in = new Scanner(System.in);
-            String input;
-            float num1 = 0;
-            float num2 = 0;
-            boolean bool = false;
+        Scanner in = new Scanner(System.in);
+        String input;
+        float num1 = 0;
+        float num2 = 0;
+        boolean bool = false;
 
 
         while (!bool)
@@ -25,7 +36,6 @@ public class foolProof {
                 {
                     bool = true;
                 }
-
                 else
                 {
                     num1 = Float.parseFloat(input);
