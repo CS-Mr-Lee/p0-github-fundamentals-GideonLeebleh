aab04763ea2a5ab7d87aa1639481af21434d7ab1
GideonLeebleh <90928962+GideonLeebleh@users.noreply.github.com>
2021-11-09 03:29:34 -0500

    Update reverse2.java

--- assignment1/reverse2.java
+++ assignment1/reverse2.java
@@ -1,30 +1,36 @@
+/**
+ *   Author name: Gideon Lee
+ *   Date: Nov 9 2021
+ *   Program name: Reverse2
+ *   Program purpose: This is a program that initialized an array of size 14 with user input and then reverses the order of the values in the array using 1 array.
+*/
 package com.company;
 import java.util.Scanner;
 
-public class reverse2 {
+public class Reverse2 {
 
     public static void main(String[] args) {
 
         //Setup
 
-            Scanner in = new Scanner(System.in);
-            int[] array = new int[14];
+        Scanner in = new Scanner(System.in);
+        int[] array = new int[14];
 
         //Input / Calculations
 
         System.out.println("Please input 14 numbers:");
 
-            for (int i = 0; i <= 13; i++)
-            {
-                array[i] = in.nextInt();
-            }
+        for (int i = 13; i >= 0; i--)
+        {
+            array[i] = in.nextInt();
+        }
 
 
         //Output
 
-            for (int i = 13; i >= 0; i--)
-            {
-                System.out.print(array[i] + " ");
-            }
+        for (int i = 0; i <= 13; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
     }
 }
