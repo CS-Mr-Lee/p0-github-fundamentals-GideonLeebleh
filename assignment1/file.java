117519de4654fd15523423c58cbb7494dcac2776
GideonLeebleh <90928962+GideonLeebleh@users.noreply.github.com>
2021-11-11 13:09:13 -0500

    Update file.java

--- assignment1/file.java
+++ assignment1/file.java
@@ -1,10 +1,16 @@
+/**
+ *   Author name: Gideon Lee
+ *   Date: Nov 11 2021
+ *   Name of program: File
+ *   Program purpose: This program will create a text file that contains a power of two table in HTML format.
+ *                    The number of values on the table will depend on the user's input.
+ */
 package com.company;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.Scanner;
-import java.io.*;
 
-public class file {
+public class File {
 
     public static void main(String[] args) {
 
@@ -17,17 +23,21 @@ public class file {
 
         try
         {
-            //to get program to work, replace Gideo with whatever your computer uses
             BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\Gideo\\Desktop\\twoPowerTable.html"));
-            bw.write("Exponent power of 2");
-            bw.write("<br>");
 
+            bw.write("<html><head>");
+            bw.write("<title>Powers of Two</title>");
+            bw.write("</head>");
+            bw.write("<body>");
+            bw.write("<table border cellpadding=5>");
+            bw.write("<tr><th>Power of 2</th><th>Value</th></tr>");
+            //loop to create the table
             for(int i = 0; i <= num; i++)
             {
-                bw.write("<pre>" + i + "             " + Math.pow(2,i) + "</pre>");
-                bw.write("<br>");
+                bw.write("<tr><th>" + i + "</th><th>" + Math.pow(2,i) + "</th></tr>");
             }
-
+            bw.write("</table>");
+            bw.write("</body></html>");
             bw.close();
         }
         catch(Exception ex)
